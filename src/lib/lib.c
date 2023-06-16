5c41f8a1ef68c798faf99e072d42804f62c75e65
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-16 13:29:41 +0530

    lib.c formed.

---
+++ src/lib/lib.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct {
+  int length;
+  int *array_pointer;
+} array;
+
+void assign_index(array *, int, int);
+int search(array *, int);
+void swap(array *, int, int);
+void left_rotate(array *);
+void right_rotate(array *);
+void cyclic_input(array *);
